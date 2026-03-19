52b1d31ef58cd3e1e7fcf114dfe434e4ec7e05cb
Allen, Timothy <tallen@wharton.upenn.edu>
2026-03-19 11:45:17 -0400

    Build dist inside assets.

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ const esbuild = require('esbuild');
 esbuild.build({
   entryPoints: ['content.js'],
   bundle: true,
-  outfile: 'dist/content.bundle.js',
+  outfile: 'assets/dist/content.bundle.js',
   external: [],
   platform: 'browser'
 }).catch(() => process.exit(1));
